de45dee90e34ff0f5cfa297bbcb1c6b9ec8ccc77
rayis <rayis@LAPTOP-4EB34VCS>
2018-12-16 00:58:15 +0800

    finial

--- src/com/exam/Order.java
+++ src/com/exam/Order.java
@@ -1,5 +1,16 @@
 package com.exam;
 
 public class Order {
-
+	Ticket [] ticket_array = new Ticket[100];
+	int ticket_idx = 0;
+	public void print() {
+		System.out.print("Result:\n");
+		for (int i = 0; i < ticket_idx; i++) {
+			System.out.print(ticket_array[i].get_result() + "\n");
+		}
+	}
+	public void add(Ticket ticket) {
+		ticket_array[ticket_idx] = ticket;
+		ticket_idx++;
+	}
 }
--- src/com/exam/Ticket.java
+++ src/com/exam/Ticket.java
@@ -1,5 +1,21 @@
 package com.exam;
 
 public class Ticket {
-
+	String origin;
+	String destination;
+	int price;
+	int quantity;
+	public Ticket(String origin, String destination, int price,int quantity) {
+		 this.origin = origin;
+		 this.destination = destination;
+		 this.price = price;
+		 this.quantity = quantity;
+	}
+	public int get_total_price() {
+		return this.price * this.quantity;
+	}
+	public String get_result() {
+		return this.origin + " " + this.destination + " "
+			  +this.price + " " + this.quantity + " " + get_total_price();
+	}
 }
